3c704f28246781109f58a981f3fc0b092bfedbbe
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2023-09-13 18:40:20 +0300

    Add BookRepository and update BookController

---
+++ src/main/java/com/example/jaakkobookstore/web/BookController.java
@@ -0,0 +1,20 @@
+package com.example.jaakkobookstore.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(Model model) {
+        
+        return "index";
+    }
+}
+
